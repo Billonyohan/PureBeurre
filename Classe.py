2f1d43a1f845276cd221eecb07dcd79ce9c0a183
Billonyohan <yohan.billon@yahoo.fr>
2019-11-25 16:48:01 +0100

    Changement fonctionnement historique
    Classe.py = création table bdd pour sauvegarder l'historique des aliments substitués
    
    Interface.py = simplification du code + changement de gestion de l'historique avec deux nouvelles tables dans la bdd openfoodfact
    
    selectmysql = insert données dans les deux nouvelles tables, plus recupération pour les afficher
    
    Pep8 sur les trois fichiers

--- Classe.py
+++ Classe.py
@@ -1,192 +1,198 @@
 # -*- coding: utf-8 -*-
 import mysql.connector
 import requests
-from pprint import pprint
 from Constant import *
 
 
-class OpenFoodFact :
+class OpenFoodFact:
+    def __init__(self):
+        self.connexion_data_base = mysql.connector.connect(user=MYSQL_USER, password=MYSQL_PWD, host=MYSQL_HOST, database=MYSQL_DATABASE)
+        self.data_base = dataBaseMySql()
+        self.data_base.delete_table()
+        self.data_base.create_data_base()
+        self.get_category()
+        self.get_food()
+        self.get_substitute()
 
+    def get_category(self):
+        '''get category from API's openfoodfacts'''
+        resp_category = requests.get('https://fr.openfoodfacts.org/categories&json=1')
+        data_category_json = resp_category.json()
+        data_tags_category = data_category_json.get('tags')
+        data_name_category = [d.get('name') for d in data_tags_category]
+        for i in range(10):
+            self.data_base.insert_data_category(data_name_category[i])
 
-	def __init__(self):
-		self.connexion_data_base = mysql.connector.connect(user=MYSQL_USER, password=MYSQL_PWD, host=MYSQL_HOST, database=MYSQL_DATABASE)
-		self.data_base = dataBaseMySql()
-		self.data_base.delete_table()
-		self.data_base.create_data_base()
-		self.get_category()
-		self.get_food()
-		self.get_substitute()
+    def get_food(self):
+        for i in range(1, 11):
+            nb_init_category = i
+            nb_food = 10
+            cursor = self.connexion_data_base.cursor()
+            nb_category = str(nb_init_category)
+            select_category = ("SELECT category FROM Category WHERE idCategory = "+nb_category)
+            cursor.execute(select_category)
+            category_saved = str(cursor.fetchone()[0])
+            parameters = {
+            'action': 'process',
+            'tagtype_0': 'categories',
+            'tag_contains_0': 'contains',
+            'tag_0': '{}'.format(category_saved),
+            'sort_by': 'unique_scans_n',
+            'page_size': '{}'.format(nb_food),
+            'countries': 'France',
+            'json': 1,
+            'page': 1
+            }
+            r_food = requests.get('https://fr.openfoodfacts.org/cgi/search.pl', params=parameters)
+            food_json = r_food.json()
+            data_food = food_json.get('products')
+            food_id = ("SELECT idCategory FROM Category WHERE idCategory = "+nb_category)
+            cursor.execute(food_id)
+            food_id_saved = cursor.fetchone()[0]
 
-	def get_category(self):
-		'''get category from API's openfoodfacts'''
-		resp_category = requests.get('https://fr.openfoodfacts.org/categories&json=1')
-		data_category_json = resp_category.json()
-		data_tags_category = data_category_json.get('tags')
-		data_name_category = [d.get('name') for d in data_tags_category]
+            for j in range(nb_food):
+                food = [d.get('product_name_fr') for d in data_food]
+                ingredients = [d.get('ingredients_text_fr') for d in data_food]
+                nutriscore = [d.get('nutriscore_grade') for d in data_food]
+                store = [d.get('stores') for d in data_food]
+                link = [d.get('link') for d in data_food]
+                self.data_base.insert_data_food(nb_category, food[j], ingredients[j], nutriscore[j], store[j], link[j])
 
+    def get_substitute(self):
 
-		for i in range(10):
-			self.data_base.insert_data_category(data_name_category[i])
+        for i in range(1, 11):
+            nb_init_substitute = i
+            nb_substitute = 35
+            cursor = self.connexion_data_base.cursor()
+            nb_category = str(nb_init_substitute)
+            select_category = ("SELECT category FROM Category WHERE idCategory = "+nb_category)
+            cursor.execute(select_category)
+            substitute_saved = str(cursor.fetchone()[0])
+            parameters = {
+            'action': 'process',
+            'tagtype_0': 'categories',
+            'tag_contains_0': 'contains',
+            'tag_0': '{}'.format(substitute_saved),
+            'sort_by': 'unique_scans_n',
+            'page_size': '{}'.format(nb_substitute),
+            'countries': 'France',
+            'json': 1,
+            'page': 1
+            }
+            request_substitute = requests.get('https://fr.openfoodfacts.org/cgi/search.pl', params=parameters)
+            subsitute_json = request_substitute.json()
+            data_substitute = subsitute_json.get('products')
+            substitute_id = ("SELECT idCategory FROM Category WHERE idCategory = "+nb_category)
+            cursor.execute(substitute_id)
+            food_id_saved = cursor.fetchone()[0]
 
-
-	def get_food(self):
-		for i in range(1,11):
-			nb_init_category = i
-			nb_food = 10
-			cursor = self.connexion_data_base.cursor()
-			nb_category = str(nb_init_category)
-			select_category = ("SELECT category FROM Category WHERE idCategory = "+nb_category)
-			cursor.execute(select_category)
-			category_saved = str(cursor.fetchone()[0])
-			parameters = {
-			'action': 'process',
-			'tagtype_0': 'categories',
-			'tag_contains_0': 'contains',
-			'tag_0': '{}'.format(category_saved),
-			'sort_by': 'unique_scans_n',
-			'page_size': '{}'.format(nb_food),
-			'countries': 'France',
-			'json': 1,
-			'page': 1
-			}
-			r_food = requests.get('https://fr.openfoodfacts.org/cgi/search.pl', params=parameters)
-			food_json = r_food.json()
-			data_food = food_json.get('products')
-
-			food_id = ("SELECT idCategory FROM Category WHERE idCategory = "+nb_category)
-			cursor.execute(food_id)
-			food_id_saved = cursor.fetchone()[0]
-
-			for j in range(nb_food) :
-				food = [d.get('product_name_fr') for d in data_food] 
-				ingredients = [d.get('ingredients_text_fr') for d in data_food]
-				nutriscore = [d.get('nutriscore_grade') for d in data_food]
-				store = [d.get('stores') for d in data_food]
-				link = [d.get('link') for d in data_food]
-				self.data_base.insert_data_food(nb_category, food[j], ingredients[j], nutriscore[j], store[j], link[j])
-
-	def get_substitute(self):
-
-		for i in range(1, 11):
-			nb_init_substitute = i
-			nb_substitute = 35
-			cursor = self.connexion_data_base.cursor()
-			nb_category = str(nb_init_substitute)
-			select_category = ("SELECT category FROM Category WHERE idCategory = "+nb_category)
-			cursor.execute(select_category)
-			substitute_saved = str(cursor.fetchone()[0])
-			parameters = {
-			'action': 'process',
-			'tagtype_0': 'categories',
-			'tag_contains_0': 'contains',
-			'tag_0': '{}'.format(substitute_saved),
-			'sort_by': 'unique_scans_n',
-			'page_size': '{}'.format(nb_substitute),
-			'countries': 'France',
-			'json': 1,
-			'page': 1
-			}
-			request_substitute = requests.get('https://fr.openfoodfacts.org/cgi/search.pl', params=parameters)
-			subsitute_json = request_substitute.json()
-			pprint(subsitute_json)
-			data_substitute = subsitute_json.get('products')
-			substitute_id = ("SELECT idCategory FROM Category WHERE idCategory = "+nb_category)
-			cursor.execute(substitute_id)
-			food_id_saved = cursor.fetchone()[0]
-
-			for j in range(10, 35):
-				food = [d.get('product_name_fr') for d in data_substitute] 
-				ingredients = [d.get('ingredients_text_fr') for d in data_substitute] 
-				nutriscore = [d.get('nutriscore_grade') for d in data_substitute]
-				store = [d.get('stores') for d in data_substitute]
-				link = [d.get('link') for d in data_substitute]
-				self.data_base.insert_substitute_food(nb_category, food[j], ingredients[j], nutriscore[j], store[j], link[j])
-
-
-
-
+            for j in range(10, 35):
+                food = [d.get('product_name_fr') for d in data_substitute]
+                ingredients = [d.get('ingredients_text_fr') for d in data_substitute]
+                nutriscore = [d.get('nutriscore_grade') for d in data_substitute]
+                store = [d.get('stores') for d in data_substitute]
+                link = [d.get('link') for d in data_substitute]
+                self.data_base.insert_substitute_food(nb_category, food[j], ingredients[j], nutriscore[j], store[j], link[j])
 
 
 class dataBaseMySql:
+    def __init__(self):
+        self.connexion_data_base = mysql.connector.connect(user=MYSQL_USER, password=MYSQL_PWD, host=MYSQL_HOST, database=MYSQL_DATABASE)
 
-		def __init__(self):
-			self.connexion_data_base = mysql.connector.connect(user=MYSQL_USER, password=MYSQL_PWD, host=MYSQL_HOST, database=MYSQL_DATABASE)
+    def delete_table(self):
+        self.cursor = self.connexion_data_base.cursor()
+        self.cursor.execute("DROP TABLE IF EXISTS Food")
+        self.cursor.execute("DROP TABLE IF EXISTS Substitute")
+        self.cursor.execute("DROP TABLE IF EXISTS Category")
+        self.cursor.execute("DROP TABLE IF EXISTS SubstituteSaved")
+        self.cursor.execute("DROP TABLE IF EXISTS FoodSaved")
+        self.connexion_data_base.commit()
+
+    def create_data_base(self):
+        self.cursor = self.connexion_data_base.cursor()
+        self.cursor.execute("""
+            CREATE TABLE Category (
+                idCategory int(15) NOT NULL AUTO_INCREMENT,
+                category varchar(100) NOT NULL,
+                PRIMARY KEY (idCategory)
+                )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+                """)
+
+        self.cursor.execute("""
+            CREATE TABLE Food (
+                id int NOT NULL AUTO_INCREMENT,
+                idCategory int(15) DEFAULT NULL,
+                food varchar(400) NOT NULL,
+                ingredients varchar(5000),
+                nutriscore varchar(10) DEFAULT NULL,
+                store varchar(1000) DEFAULT NULL,
+                link varchar(1000) DEFAULT NULL,
+                PRIMARY KEY (id)
+                )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+                """)
+
+        self.cursor.execute("""
+            CREATE TABLE Substitute (
+                id int(11) NOT NULL AUTO_INCREMENT,
+                idCategory int(15) DEFAULT NULL,
+                substitute varchar(100) DEFAULT NULL,
+                ingredients varchar(5000) DEFAULT NULL,
+                nutriscore varchar(10) DEFAULT NULL,
+                store varchar(1000) DEFAULT NULL,
+                link varchar(1000) DEFAULT NULL,
+                PRIMARY KEY (id)
+                )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+                """)
+
+        self.cursor.execute("""
+            CREATE TABLE SubstituteSaved (
+                id int(11) NOT NULL AUTO_INCREMENT,
+                substitute varchar(100) DEFAULT NULL,
+                ingredients varchar(5000) DEFAULT NULL,
+                nutriscore varchar(10) DEFAULT NULL,
+                store varchar(1000) DEFAULT NULL,
+                link varchar(1000) DEFAULT NULL,
+                PRIMARY KEY (id)
+                )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+                """)
+
+        self.cursor.execute("""
+            CREATE TABLE FoodSaved (
+                id int(11) NOT NULL AUTO_INCREMENT,
+                food varchar(400) NOT NULL,
+                ingredients varchar(5000),
+                nutriscore varchar(10) DEFAULT NULL,
+                store varchar(1000) DEFAULT NULL,
+                link varchar(1000) DEFAULT NULL,
+                PRIMARY KEY (id)
+                )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+                """)
+
+        self.connexion_data_base.commit()
+        self.cursor.close()
+
+    def insert_data_category(self, category):
+        self.cursor = self.connexion_data_base.cursor()
+        add_category = (""" INSERT INTO Category (category)
+                            VALUES("{name_category}")""".format(name_category=category))
+        self.cursor.execute(add_category)
+        self.connexion_data_base.commit()
+        self.cursor.close()
+    def insert_data_food(self, idCategory, food, ingredients, nutriscore, store, link):
+        self.cursor = self.connexion_data_base.cursor()
+        add_food = (""" INSERT INTO Food (idCategory, food, ingredients, nutriscore, store, link)
+                        VALUES("{cat_save}", "{name_food}", "{nb_ingredients}", "{nutri_score}", "{nb_store}", "{product_link}")""".format(cat_save=idCategory, name_food=food, nb_ingredients=ingredients, nutri_score=nutriscore, nb_store=store, product_link=link))
+        self.cursor.execute(add_food)
+        self.connexion_data_base.commit()
+        self.cursor.close()
+
+    def insert_substitute_food(self, idCategory, food, ingredients, nutriscore, store, link):
+        self.cursor = self.connexion_data_base.cursor()
+        add_substitute = (""" INSERT INTO Substitute (idCategory, substitute, ingredients, nutriscore,  store, link)
+                            VALUES("{category_save}", "{name_food}", "{nb_ingredients}", "{nutri_score}", "{nb_store}", "{product_link}")""".format(category_save=idCategory, name_food=food, nb_ingredients=ingredients, nutri_score=nutriscore, nb_store=store, product_link=link))
+        self.cursor.execute(add_substitute)
+        self.connexion_data_base.commit()
+        self.cursor.close()
 
 
-
-		def delete_table(self):
-			self.cursor = self.connexion_data_base.cursor()
-			self.cursor.execute("DROP TABLE IF EXISTS Food")
-			self.cursor.execute("DROP TABLE IF EXISTS Substitute")
-			self.cursor.execute("DROP TABLE IF EXISTS Category")
-				
-			self.connexion_data_base.commit()
-
-
-		def create_data_base(self):
-			self.cursor = self.connexion_data_base.cursor()
-			self.cursor.execute("""
-				CREATE TABLE Category (
-					idCategory int(11) NOT NULL AUTO_INCREMENT,
-					category varchar(1000) NOT NULL,
-					PRIMARY KEY (idCategory)
-					)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-					""")
-
-			self.cursor.execute("""
-				CREATE TABLE Food (
-					id int NOT NULL AUTO_INCREMENT,
-					idCategory int(15) DEFAULT NULL, 
-					food varchar(400) NOT NULL,
-					ingredients varchar(6000),
-					nutriscore varchar(10) DEFAULT NULL,
-					store varchar(1000) DEFAULT NULL,
-					link varchar(1000) DEFAULT NULL,
-					PRIMARY KEY (id)
-					)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-					""")
-
-			self.cursor.execute("""
-				CREATE TABLE Substitute (
-					id int(11) NOT NULL AUTO_INCREMENT,
-					idCategory int(15) DEFAULT NULL,
-					substitute varchar(100) DEFAULT NULL,
-					ingredients varchar(5000) DEFAULT NULL,
-					nutriscore varchar(10) DEFAULT NULL,
-					store varchar(1000) DEFAULT NULL,
-					link varchar(1000) DEFAULT NULL,
-					PRIMARY KEY (id)
-					)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-					""")
-
-			self.connexion_data_base.commit()
-			self.cursor.close()
-
-		def insert_data_category(self, category):
-			self.cursor = self.connexion_data_base.cursor()
-			add_category = (""" INSERT INTO Category (category)
-								VALUES("{name_category}")""".format(name_category = category))
-			self.cursor.execute(add_category)
-			self.connexion_data_base.commit()
-			self.cursor.close()
-
-		def insert_data_food(self, idCategory, food, ingredients, nutriscore, store, link):
-			self.cursor = self.connexion_data_base.cursor()
-			add_food = (""" INSERT INTO Food (idCategory, food, ingredients, nutriscore, store, link)
-								VALUES("{cat_save}", "{name_food}", "{nb_ingredients}", "{nutri_score}", "{nb_store}", "{product_link}")""".format(cat_save=idCategory, name_food=food, nb_ingredients=ingredients, nutri_score=nutriscore, nb_store=store, product_link=link))
-			
-			self.cursor.execute(add_food)
-			self.connexion_data_base.commit()
-			self.cursor.close()
-
-		def insert_substitute_food(self, idCategory, food, ingredients, nutriscore, store, link):
-			self.cursor = self.connexion_data_base.cursor()
-			add_substitute = (""" INSERT INTO Substitute (idCategory, substitute, ingredients, nutriscore,  store, link)
-								VALUES("{category_save}", "{name_food}", "{nb_ingredients}", "{nutri_score}", "{nb_store}", "{product_link}")""".format(category_save=idCategory, name_food=food, nb_ingredients=ingredients, nutri_score=nutriscore, nb_store=store, product_link=link))
-			self.cursor.execute(add_substitute)
-			self.connexion_data_base.commit()
-			self.cursor.close()
-
-
-
-create_data_base =  OpenFoodFact()
+create_data_base = OpenFoodFact()
--- SelectMysql.py
+++ SelectMysql.py
@@ -1,79 +1,144 @@
 import mysql.connector
 from Constant import *
 
+
 class mysql_select:
+    def __init__(self):
+        self.connexion_data_base = mysql.connector.connect(user=MYSQL_USER, password=MYSQL_PWD, host=MYSQL_HOST, database=MYSQL_DATABASE)
+        self.cursor = self.connexion_data_base.cursor()
 
-	def __init__(self):
-		self.connexion_data_base = mysql.connector.connect(user=MYSQL_USER, password=MYSQL_PWD, host=MYSQL_HOST, database=MYSQL_DATABASE)
-		self.cursor = self.connexion_data_base.cursor()	
+    def select_category(self):
+        self.cursor.execute("SELECT category FROM Category")
+        self.data_category = self.cursor.fetchall()
+        self.data_category = [d[0] for d in self.data_category]
+        return self.data_category
 
-	def select_category(self):
-		self.cursor.execute("SELECT category FROM Category")
-		self.data_category = self.cursor.fetchall()
-		self.data_category = [d[0] for d in self.data_category] 
-		return self.data_category
+    def select_food(self, idCategory):
+        self.cursor.execute("SELECT food FROM Food WHERE idCategory ="+idCategory)
+        self.data_food = self.cursor.fetchall()
+        self.data_food = [d[0] for d in self.data_food]
+        return self.data_food
 
-	def select_food(self, idCategory):
-		self.cursor.execute("SELECT food FROM Food WHERE idCategory ="+idCategory)
-		self.data_food = self.cursor.fetchall()
-		self.data_food = [d[0] for d in self.data_food]
-		return self.data_food
+    def ingredients_food(self, index):
+        self.cursor.execute("SELECT ingredients FROM Food WHERE id ="+index)
+        self.data_ingredients = self.cursor.fetchall()
+        self.data_ingredients = [d[0] for d in self.data_ingredients]
+        return self.data_ingredients
 
-	def ingredients_food(self, index):
-		self.cursor.execute("SELECT ingredients FROM Food WHERE id ="+index)
-		self.data_ingredients = self.cursor.fetchall()
-		self.data_ingredients = [d[0] for d in self.data_ingredients]
-		return self.data_ingredients
+    def nutriscore_food(self, index):
+        self.cursor.execute("SELECT nutriscore FROM Food WHERE id="+index)
+        self.data_nutriscore = self.cursor.fetchall()
+        self.data_nutriscore = [d[0] for d in self.data_nutriscore]
+        return self.data_nutriscore
 
-	def nutriscore_food(self, index):
-		self.cursor.execute("SELECT nutriscore FROM Food WHERE id="+index)
-		self.data_nutriscore = self.cursor.fetchall()
-		self.data_nutriscore = [d[0] for d in self.data_nutriscore]
-		return self.data_nutriscore
+    def store_food(self, index):
+        self.cursor.execute("SELECT store FROM food WHERE id ="+index)
+        self.data_store = self.cursor.fetchall()
+        self.data_store = [d[0] for d in self.data_store]
+        return self.data_store
 
-	def store_food(self, index):
-		self.cursor.execute("SELECT store FROM substitute WHERE id ="+index)
-		self.data_store = self.cursor.fetchall()
-		self.data_store = [d[0] for d in self.data_store]
-		return self.data_store
+    def link_food(self, index):
+        self.cursor.execute("SELECT link FROM food WHERE id ="+index)
+        self.data_link = self.cursor.fetchall()
+        self.data_link = [d[0] for d in self.data_link]
+        return self.data_link
 
-	def link_food(self, index):
-		self.cursor.execute("SELECT link FROM substitute WHERE id ="+index)
-		self.data_link = self.cursor.fetchall()
-		self.data_link = [d[0] for d in self.data_link]
-		return self.data_link
+    def select_substitute(self, idCategory):
+        self.cursor.execute("SELECT substitute FROM Substitute WHERE idCategory="+idCategory)
+        self.data_susbstitute = self.cursor.fetchall()
+        self.data_susbstitute = [d[0] for d in self.data_susbstitute]
+        return self.data_susbstitute
 
-	def select_substitute(self, idCategory):
-		self.cursor.execute("SELECT substitute FROM Substitute WHERE idCategory="+idCategory)
-		self.data_susbstitute = self.cursor.fetchall()
-		self.data_susbstitute = [d[0] for d in self.data_susbstitute]
-		return self.data_susbstitute
+    def ingredients_substitute(self, index):
+        self.cursor.execute("SELECT ingredients FROM substitute WHERE id ="+index)
+        self.data_ingredients = self.cursor.fetchall()
+        self.data_ingredients = [d[0] for d in self.data_ingredients]
+        return self.data_ingredients
 
-	def ingredients_substitute(self, index):
-		self.cursor.execute("SELECT ingredients FROM substitute WHERE id ="+index)
-		self.data_ingredients = self.cursor.fetchall()
-		self.data_ingredients = [d[0] for d in self.data_ingredients]
-		return self.data_ingredients
+    def nutriscore_substitute(self, index):
+        self.cursor.execute("SELECT nutriscore FROM Substitute WHERE id="+index)
+        self.data_nutriscore = self.cursor.fetchall()
+        self.data_nutriscore = [d[0] for d in self.data_nutriscore]
+        return self.data_nutriscore
 
-	def nutriscore_substitute(self, index):
-		self.cursor.execute("SELECT nutriscore FROM Substitute WHERE id="+index)
-		self.data_nutriscore = self.cursor.fetchall()
-		self.data_nutriscore = [d[0] for d in self.data_nutriscore]
-		return self.data_nutriscore
+    def store_substitute(self, index):
+        self.cursor.execute("SELECT store FROM substitute WHERE id ="+index)
+        self.data_store = self.cursor.fetchall()
+        self.data_store = [d[0] for d in self.data_store]
+        return self.data_store
 
-	def store_substitute(self, index):
-		self.cursor.execute("SELECT store FROM substitute WHERE id ="+index)
-		self.data_store = self.cursor.fetchall()
-		self.data_store = [d[0] for d in self.data_store]
-		return self.data_store
+    def link_substitute(self, index):
+        self.cursor.execute("SELECT link FROM substitute WHERE id ="+index)
+        self.data_link = self.cursor.fetchall()
+        self.data_link = [d[0] for d in self.data_link]
+        return self.data_link
 
-	def link_substitute(self, index):
-		self.cursor.execute("SELECT link FROM substitute WHERE id ="+index)
-		self.data_link = self.cursor.fetchall()
-		self.data_link = [d[0] for d in self.data_link]
-		return self.data_link
+    def substitute_saved(self, idSubstitute):
+        add_sub = (" INSERT INTO SubstituteSaved (substitute, ingredients, nutriscore, store, link) SELECT substitute, ingredients, nutriscore, store, link FROM Substitute WHERE id ="+idSubstitute)
+        self.cursor.execute(add_sub)
+        self.connexion_data_base.commit()
 
+    def food_saved(self, idFood):
+        add_fo = ("INSERT INTO FoodSaved (food, ingredients, nutriscore, store, link) SELECT food, ingredients, nutriscore, store, link FROM Food WHERE id ="+idFood)
+        self.cursor.execute(add_fo)
+        self.connexion_data_base.commit()
 
+    def food_save(self, index):
+        self.cursor.execute("SELECT food FROM FoodSaved WHERE id="+index)
+        self.data_food = self.cursor.fetchall()
+        self.data_food = [d[0] for d in self.data_food]
+        return self.data_food
 
+    def ingredients_food_saved(self, index):
+        self.cursor.execute("SELECT ingredients FROM FoodSaved WHERE id ="+index)
+        self.data_ingredients = self.cursor.fetchall()
+        self.data_ingredients = [d[0] for d in self.data_ingredients]
+        return self.data_ingredients
 
+    def nutriscore_food_saved(self, index):
+        self.cursor.execute("SELECT nutriscore FROM FoodSaved WHERE id="+index)
+        self.data_nutriscore = self.cursor.fetchall()
+        self.data_nutriscore = [d[0] for d in self.data_nutriscore]
+        return self.data_nutriscore
 
+    def store_food_saved(self, index):
+        self.cursor.execute("SELECT store FROM FoodSaved WHERE id ="+index)
+        self.data_store = self.cursor.fetchall()
+        self.data_store = [d[0] for d in self.data_store]
+        return self.data_store
+
+    def link_food_saved(self, index):
+        self.cursor.execute("SELECT link FROM FoodSaved WHERE id ="+index)
+        self.data_link = self.cursor.fetchall()
+        self.data_link = [d[0] for d in self.data_link]
+        return self.data_link
+
+    def substitute_save(self):
+        self.cursor.execute("SELECT substitute FROM SubstituteSaved")
+        self.data_susbstitute = self.cursor.fetchall()
+        self.data_susbstitute = [d[0] for d in self.data_susbstitute]
+        return self.data_susbstitute
+
+    def ingredients_substitute_saved(self, index):
+        self.cursor.execute("SELECT ingredients FROM SubstituteSaved WHERE id ="+index)
+        self.data_ingredients = self.cursor.fetchall()
+        self.data_ingredients = [d[0] for d in self.data_ingredients]
+        return self.data_ingredients
+
+    def nutriscore_substitute_saved(self, index):
+        self.cursor.execute("SELECT nutriscore FROM SubstituteSaved WHERE id="+index)
+        self.data_nutriscore = self.cursor.fetchall()
+        self.data_nutriscore = [d[0] for d in self.data_nutriscore]
+        return self.data_nutriscore
+
+    def store_substitute_saved(self, index):
+        self.cursor.execute("SELECT store FROM SubstituteSaved WHERE id ="+index)
+        self.data_store = self.cursor.fetchall()
+        self.data_store = [d[0] for d in self.data_store]
+        return self.data_store
+
+    def link_substitute_saved(self, index):
+        self.cursor.execute("SELECT link FROM SubstituteSaved WHERE id ="+index)
+        self.data_link = self.cursor.fetchall()
+        self.data_link = [d[0] for d in self.data_link]
+        return self.data_link
